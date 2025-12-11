a41e2828c635a48218a065bd219af38735e6808a
Nitesh Sharma <niteshsharma25201@gmail.com>
2025-12-11 18:27:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5850472675-388de",
+  "appId": "1:248514351454:web:deccbb6034c0fea0b57fcf",
+  "apiKey": "AIzaSyAfuXAz9MecXr65GBFzOdaOiZI2jaJDopw",
+  "authDomain": "studio-5850472675-388de.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "248514351454"
+};
