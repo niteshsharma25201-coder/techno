96eac96d6b80ef903dcf4f4cde398adf5001991a
Nitesh Sharma <niteshsharma25201@gmail.com>
2025-12-12 08:08:38 +0000

    show product in grid view

--- src/components/products/product-list.tsx
+++ src/components/products/product-list.tsx
@@ -31,7 +31,7 @@ export default function ProductList({ allProducts }: ProductListProps) {
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
       {filteredProducts.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
