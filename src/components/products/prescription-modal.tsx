262986be153972fbede40399ae559ffb5898ea19
Nitesh Sharma <niteshsharma25201@gmail.com>
2025-12-13 10:37:10 +0000

    in progessive lens manual enter eye vision in second section remove (ADD

--- src/components/products/prescription-modal.tsx
+++ src/components/products/prescription-modal.tsx
@@ -115,7 +115,7 @@ export default function PrescriptionModal({
               {isProgressiveOrBifocal && (
                 <>
                     <div className="space-y-2">
-                        <Label htmlFor="right-eye-nv">Right Eye (ADD)</Label>
+                        <Label htmlFor="right-eye-nv">Right Eye</Label>
                         <Input
                         id="right-eye-nv"
                         placeholder="e.g., +1.25"
@@ -124,7 +124,7 @@ export default function PrescriptionModal({
                         />
                     </div>
                     <div className="space-y-2">
-                        <Label htmlFor="left-eye-nv">Left Eye (ADD)</Label>
+                        <Label htmlFor="left-eye-nv">Left Eye</Label>
                         <Input
                         id="left-eye-nv"
                         placeholder="e.g., +1.50"
