ecae27dc0f1a8acc3d9bb9d2186b0ca8c3acb200
Nitesh Sharma <niteshsharma25201@gmail.com>
2025-12-14 06:52:19 +0000

    all feature is wroking and when i diploy the features in versel but is i

--- next.config.ts
+++ next.config.ts
@@ -10,12 +10,6 @@ const pwaConfig = withPWA({
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
